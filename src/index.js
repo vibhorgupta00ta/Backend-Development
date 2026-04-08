53b14da9dea7accdf936647e8ba70308faa7ad2b
vibhor <vibhorg62@gmail.com>
2026-04-08 18:46:52 +0530

    router controller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-
+import {app} from './app.js'
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
-
+.then(()=>{
+    app.on("error",()=>{
+        console.log("Error",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MONGO DB connection failed",err);
+    
+})
 
 
 
